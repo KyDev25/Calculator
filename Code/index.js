8e04a1158af0389769980d134248c36634a6b2cb
Ky_Dev25 <kylian.barthelemy2003@gmail.com>
2022-11-22 15:21:21 +0100

    ajout des commentaires

--- Code/index.js
+++ Code/index.js
@@ -1,23 +1,23 @@
 const buttons = document.querySelectorAll(".btn");
 const result = document.getElementById("result");
 
+//Get result when we clicked on a button
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     result.textContent += e.target.id;
   });
 });
 
+//Calculate the result
 equal.addEventListener("click", () => {
-    try {
-        result.textContent = eval(result.textContent);
-    }
-    catch (SyntaxError) {
-        result.textContent = "ERROR"
-    }
+  try {
+    result.textContent = eval(result.textContent);
+  } catch (SyntaxError) {
+    result.textContent = "ERROR";
+  }
 });
 
+//Clear the result
 clear.addEventListener("click", () => {
   result.textContent = result.textContent.clear;
 });
-
-
